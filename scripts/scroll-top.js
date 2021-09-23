61cd63468f70fedfb60c50930c4afc365d6b8e1f
Alexey Shevchenko <88481806+AleksSh-itp@users.noreply.github.com>
2021-09-23 13:27:11 +0300

    Update scroll-top.js

--- scripts/scroll-top.js
+++ scripts/scroll-top.js
@@ -5,19 +5,15 @@ article.onscroll = function(e) {
     let scrolled = article.scrollTop;
     var coords =  article.offsetHeight;
 
-    if (scrolled > coords)
-    {
+    if (scrolled > coords) {
         toTopButton.parentElement.classList.add("to-top-show");
-    }
-    else
-    {
+    } else {
         toTopButton.parentElement.classList.remove("to-top-show");
     }
 }
 
 toTopButton.onclick = function() {
-    if (article.offsetWidth > 0)
-    {
+    if (article.offsetWidth > 0) {
         article.scrollTo({
             top: 0,
             behavior: "smooth"
